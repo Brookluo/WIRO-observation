9de6a6f034b7c3a2b6726c80e2afdc1835b9a56d
Yufeng Luo <brookluo@yahoo.com>
2022-09-26 22:45:10 -0600

    pointing direction generation

--- src/plan_obs.py
+++ src/plan_obs.py
@@ -3,6 +3,7 @@ from astropy.coordinates import AltAz, EarthLocation, SkyCoord
 from astropy.time import Time
 from astropy.units.quantity import Quantity
 from astroplan.scheduling import Scheduler
+import numpy as np
 
 
 def output_minsecz_in_month(month: int, year: int, obs_loc: EarthLocation, obj_coords: SkyCoord):
@@ -112,7 +113,7 @@ def schedule_obs_for_one_day(month: int, day: int, targets: SkyCoord, scheduler:
     
     blocks = []
     full_night_start = Time(f"2022-{month}-{day} 19:00") - utcoffset
-    full_night_end = Time(f"2022-{month}-{day} 4:00") + 1 * u.day - utcoffset
+    full_night_end = Time(f"2022-{month}-{day} 6:00") + 1 * u.day - utcoffset
     full_night = TimeConstraint(full_night_start, full_night_end)
     # Create ObservingBlocks for each filter and target with our time
     # constraint, and durations determined by the exposures needed
@@ -136,3 +137,113 @@ def schedule_obs_for_one_day(month: int, day: int, targets: SkyCoord, scheduler:
     # Call the schedule with the observing blocks and schedule to schedule the blocks
     scheduler(blocks, day_schedule)
     return day_schedule
+
+
+def write_plan_to_file(filename, ondate, utcoffset, obs_constraints,
+                       target_coord, site_location, site_name):
+    """_summary_
+
+    Parameters
+    ----------
+    filename : _type_
+        _description_
+    ondate : _type_
+        _description_
+    utcoffset : _type_
+        _description_
+    target_coord : astropy.coordinates.SkyCoord
+        SkyCoord of the target (must be a single target)
+    site_location : _type_
+        _description_
+    site_name : _type_
+        _description_
+    """    
+    from astroplan import Observer, observability_table
+    
+    lines = ["UTC, LocalTime, airmass, obs_frac\n"]
+    ten_min = 1 * u.hour / 6
+    observer = Observer(location=site_location, name=site_name)
+    for i in range(23 * 6):
+        new_date = ondate + ten_min
+        time_range = Time([ondate, new_date])
+        obs_table = observability_table(obs_constraints, observer, 
+                                        [target_coord], time_range=time_range, 
+                                        time_grid_resolution=1*u.minute)
+        airmass = target_coord.transform_to(AltAz(obstime=ondate, location=site_location)).secz
+        obs_frac = obs_table["fraction of time observable"][0]
+        utc = ondate
+        lst = ondate + utcoffset
+        if obs_table["fraction of time observable"][0] > 0:
+            lines.append(f"{utc}, {lst}, {airmass}, {obs_frac}\n")
+        ondate = new_date
+    with open(filename, "w") as fh:
+        fh.writelines(lines)
+        
+        
+def generate_pointing_directions(field_FOV_x, field_FOV_y, detector_FOV_x, detector_FOV_y,
+                            finest_level, overlap_frac, center_ra, center_dec, 
+                            radius=None):
+    """Generate pointing directions given a field of view of the survey field and 
+    a detector field of view. The pointing directions are generated in a grid pattern
+    to satisfy the dithering pattern. Note that the we can always use a rotation matrix
+    to rotate the field if needed, so FOV is really just a 2D angular separation
+
+    Parameters
+    ----------
+    field_FOV_x : astropy.units.Quantity
+        Field of view in the x direction of the survey field.
+    field_FOV_y : astropy.units.Quantity
+        Field of view in the y direction of the survey field.
+    detector_FOV_x : astropy.units.Quantity
+        Field of view in the x direction of the detector.
+    detector_FOV_y : astropy.units.Quantity
+        Field of view in the y direction of the detector.
+    finest_level : int
+        Finest level of the pointing grid. This is the number of points in the x and y. 
+        This is 2^(n-1), where n is the number of levels in the grid.
+    overlap_frac : float
+        how much of each detector field of view is overlapping. This is the factor
+        determines the dithering path.
+    center_ra : astropy.units.Quantity
+        Center RA of the field.
+    center_dec : astropy.units.Quantity
+        Center Dec of the field.
+    radius : astropy.units.Quantity, optional
+        the radius of the circle to constrain the field (has a 5% oversize), by default None
+
+    Returns
+    -------
+    ra, dec : two 1D-arrays of astropy.units.Quantity
+        RA and Dec of the pointing directions.
+    """    
+    # survey_field_x, survey_field_y = field_FOV_ra, field_FOV_dec
+    # detector_FOV_x, detector_y = detector_FOV_ra, detector_FOV_dec
+    # overlap for dithering
+    overlap_x = overlap_frac * detector_FOV_x
+    overlap_y = overlap_frac * detector_FOV_y
+    # the finest level the grid is
+    # n is essentially the refinement level
+    # number of grid is 2^(n-1)
+    shift_x = (detector_FOV_x - overlap_x) / 2**(finest_level-1)
+    shift_y = (detector_FOV_y - overlap_y) / 2**(finest_level-1)
+    num_shift_x = int(np.ceil((field_FOV_x / shift_x).decompose()))
+    num_shift_y = int(np.ceil((field_FOV_y / shift_y).decompose()))
+    adjusted_field_x = num_shift_x * shift_x
+    adjusted_field_y = num_shift_y * shift_y
+    # use symmetric padding, i.e. pad the same amount on both sides
+    padding_x = adjusted_field_x - field_FOV_x
+    padding_y = adjusted_field_y - field_FOV_y
+    xi, yi = np.meshgrid(np.linspace(-adjusted_field_x/2, adjusted_field_x/2, num_shift_x), 
+                        np.linspace(-adjusted_field_y/2, adjusted_field_y/2, num_shift_y))
+    # symmetric padding
+    xi -= padding_x/2
+    yi -= padding_y/2
+    if radius:
+        # need to add the circular rejection
+        # add a 5% oversize to compensate the loss of image on the side
+        in_circle = np.sqrt(xi**2 + yi**2) < radius*1.05
+        xi = xi[in_circle]
+        yi = yi[in_circle]
+    ra = xi.ravel() + center_ra
+    dec = yi.ravel() + center_dec
+    return ra, dec
